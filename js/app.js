de1049a493e0c31c03592de4c506c2e3010f4882
Anandhu M S <anandhumeleveettil@gmail.com>
2022-01-12 09:25:24 +0530

    v1 updated-6

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 window.addEventListener("scroll", () => {
     skillsEffect();
-})
+});
 
 function checkScroll(el){
     let rect = el.getBoundingClientRect();
@@ -16,3 +19,19 @@ function skillsEffect(){
     if(!checkScroll(skills_wrap)) return;
     skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
 }
+
+function closemenu(){
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+    if(!navbar.classList.contains("open")) {
+        navbar.classList.add("open");
+        document.body.classList.add("stop-scrolling");
+    } else {
+        closemenu();
+    }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closemenu()));
